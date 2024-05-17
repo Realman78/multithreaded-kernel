d93cb34abab2d19664135976195ae702b9e9accb
Realman78 <marin.dedic@me.com>
2024-05-17 10:36:25 +0200

    Terminal printing

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,72 @@
 #include "kernel.h"
+#include <stddef.h>
+#include <stdint.h>
 
-void kernel_main() {
+uint16_t *video_mem = 0;
+uint16_t terminal_row = 0;
+uint16_t terminal_col = 0;
 
+uint16_t terminal_make_char(char c, char color)
+{
+    return (color << 8) | c;
+}
+
+void terminal_putchar(int x, int y, char c, char color)
+{
+    video_mem[(y * VGA_WIDTH) + x] = terminal_make_char(c, color);
+}
+
+void terminal_writechar(char c, char color)
+{
+    if (c == '\n')
+    {
+        terminal_row += 1;
+        terminal_col = 0;
+        return;
+    }
+
+    terminal_putchar(terminal_col, terminal_row, c, color);
+    terminal_col += 1;
+    if (terminal_col >= VGA_WIDTH)
+    {
+        terminal_col = 0;
+        terminal_row += 1;
+    }
+}
+
+void termianl_initialize()
+{
+    video_mem = (uint16_t *)(0xB8000);
+    terminal_row = 0;
+    terminal_col = 0;
+    for (int j = 0; j < VGA_HEIGHT; j++)
+    {
+        for (int i = 0; i < VGA_WIDTH; i++)
+        {
+            terminal_putchar(i, j, ' ', 0);
+        }
+    }
+}
+
+size_t strlen(const char* str) {
+    size_t len = 0;
+    while (str[len]) len++;
+    return len;
+}
+
+
+void print(const char* str)
+{
+    size_t len = strlen(str);
+    for (int i = 0; i < len; i++)
+    {
+        terminal_writechar(str[i], 15);
+    }
+}
+
+void kernel_main()
+{
+    termianl_initialize();
+    print("Marin parin");
+    // pošto se koristi little endian, moras prvo definirat boju pa character
 }
